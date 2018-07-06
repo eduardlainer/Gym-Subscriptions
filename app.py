d30546578cf6e9bce4be08a88e2254c914b24d76
eduardlainer <eduardlainer@gmail.com>
2018-07-07 02:16:21 +0300

    Project is done, uploaded whole project

--- app.py
+++ app.py
@@ -7,26 +7,90 @@ app = Flask(__name__)
 
 db = database.Database()
 
+
 # Routes
 
 
 @app.route('/')
 def index():
     """ Dashboard """
-    return render_template("index.html", totalSubscribers=db.total_subscribers())
+    return render_template("index.html", totalSubscribers=db.total_subscribers(),
+                           activeSubs=db.totalactivesubscription(), expiredSubs=db.totalexpiredsubscription())
 
 
-@app.route('/newsubscription', methods=['POST', 'GET'])
+@app.route('/newsubscription')
 def newsubscription():
-    """ Add a new subscription page """
+    """ new subscription page """
     return render_template("newsubscription.html")
+
+
+@app.route('/existingsubscriber')
+def existingsubscriber():
+    """ existing subscription page """
+    return render_template("existingsubscriber.html", subscribers=db.getsubscribers())
+
+
+@app.route('/checksubscription')
+def checksubscription():
+    """ subscription status page """
+    return render_template("checksubscription.html")
+
+
+# Resolving routes
+
+
+@app.route('/checksub', methods=['POST', 'GET'])
+def checksub():
+    """
+    Check subscription status
+    """
+
+    #  Get info from form
+
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+
+    # Check subscription
+
+    subcheck = db.checksubscription(firstname, lastname)
+    subexist = db.subscriber_exist(firstname, lastname)
+    if subexist is False:
+        return render_template('/error.html', message="Subscriber doesn't exist!")
+
+    subcheckalert = db.checksubscriptionalert(firstname, lastname)
+    if subcheckalert == 1:
+        message = "Subscription expired!"
+        classalert = "alert-danger"
+    else:
+        message = "Subscription is ok"
+        classalert = "alert-success"
+    return render_template('/checksubscription.html', subcheck=subcheck, message=message, classalert=classalert)
+
+
+@app.route('/existingsub', methods=['POST', 'GET'])
+def existingsub():
+    """
+    Renew an subscription of an old suberiber
+    """
+
+    # Get info from form
+
+    subscriber_id = int(request.form.get('subscriber_id'))
+    creationdate = request.form.get('creationdate')
+    expirationdate = request.form.get('expirationdate')
+
+    # Renew subscription
+
+    db.renewsubscription(subscriber_id, creationdate, expirationdate)
+    return render_template("success.html", message="Subscription renewed!")
+
 
 
 @app.route("/newsubscriber", methods=['POST', 'GET'])
 def newsubscriber():
     """ Add a new subscriber """
 
-    #  Get info from html page
+    #  Get info from form
 
     firstname = request.form.get('firstname')
     lastname = request.form.get('lastname')
@@ -35,12 +99,16 @@ def newsubscriber():
     creationdate = request.form.get('creationdate')
     expirationdate = request.form.get('expirationdate')
 
-    if db.subscriber_exist(firstname):
+    # Check if subscriber already exist
+
+    if db.subscriber_exist(firstname, lastname):
         return render_template("error.html",
                                message="Subscriber already exist! Got to existing subscribers and renew the subsciption.")
 
+    # If subscriber doesn't exist add
+
     db.addsubscriber(firstname, lastname, phone, email, creationdate, expirationdate)
-    return render_template("succes.html", message="You added a new subscription.")
+    return render_template("success.html", message="You added a new subscription.")
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 import mysql.connector
+from flask import render_template
 
 
 class Database:
-
     # Database connection and cursor
 
     connection = mysql.connector.connect(user='root', host='127.0.0.1', database='gymsubs_flask')
@@ -18,13 +18,14 @@ class Database:
         totalsubscribers = self.cursor.fetchone()[0]
         return totalsubscribers
 
-    def subscriber_exist(self, fn):
+    def subscriber_exist(self, fn, ln):
         """
         See if a subscriber already exist
         """
         query = ("SELECT COUNT(*) FROM subscribers "
-                 "WHERE firstname=%s")
-        self.cursor.execute(query, (fn,))
+                 "WHERE firstname=%s "
+                 "AND lastname=%s")
+        self.cursor.execute(query, (fn, ln))
         result = self.cursor.fetchone()[0]
         if result > 0:
             return True
@@ -54,3 +55,70 @@ class Database:
                   "VALUES (%s,%s,%s)")
         self.cursor.execute(query2, (subid, cd, ed))
         self.connection.commit()
+
+    def totalactivesubscription(self):
+        """
+        Check for active subscription
+        :return:
+        """
+        query = ("SELECT COUNT(*) FROM subscription "
+                 "WHERE expiration_date > currents_date")
+        self.cursor.execute(query)
+        activeSubs = self.cursor.fetchone()[0]
+        return activeSubs
+
+    def totalexpiredsubscription(self):
+        """
+        Check for expired subscriptions
+        """
+        query = ("SELECT COUNT(*) FROM subscription "
+                 "WHERE expiration_date < currents_date")
+        self.cursor.execute(query)
+        expiredSubs = self.cursor.fetchone()[0]
+        return expiredSubs
+
+    def getsubscribers(self):
+        """
+        Get all subscribers
+        """
+        query = ("SELECT id,firstname,lastname FROM subscribers")
+        self.cursor.execute(query)
+        subs = self.cursor.fetchall()
+        lista = []
+        for sub in subs:
+            lista.append(sub)
+        return lista
+
+    def renewsubscription(self, id, cd, ed):
+        """
+        Renew subscription of an old subscriber
+        """
+        query = ("UPDATE subscription "
+                 "SET creation_date=%s, expiration_date=%s "
+                 "WHERE subscribers_id=%s")
+        self.cursor.execute(query, (cd, ed, id))
+        self.connection.commit()
+
+    def checksubscription(self, fn, ln):
+        """
+        Check a subscription
+        """
+        query = ("SELECT creation_date, expiration_date "
+                 "FROM subscription INNER JOIN subscribers "
+                 "ON subscribers_id = subscribers.id "
+                 "WHERE (subscribers.firstname=%s "
+                 "AND subscribers.lastname=%s);")
+        self.cursor.execute(query, (fn, ln))
+        sub = self.cursor.fetchall()
+        return sub
+
+    def checksubscriptionalert(self, fn, ln):
+        """
+        Check a subscription
+        """
+        query2 = ("SELECT COUNT(*) FROM subscription "
+                  "INNER JOIN subscribers ON subscribers_id=subscribers.id "
+                  "WHERE (expiration_date < currents_date) AND (subscribers.firstname=%s AND subscribers.lastname=%s)")
+        self.cursor.execute(query2, (fn, ln))
+        sub = self.cursor.fetchone()[0]
+        return sub
